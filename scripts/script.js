03f2ec4e6d4f7927b14b4ab22e48e0a20d19ca1e
rG <12301raghavgoyal@gmail.com>
2020-10-20 23:16:11 +0530

    Add typewriter effect & change layout

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+//jshint esversion:6
+const instance = new Typewriter('.taglines h3', {
+    strings: ['Take your services at fingertips.', 'Smart solution for smart users.'],
+    autoStart: true,
+    loop: true,
+    cursor: "",
+    cursorClassName: "cursor",
+    delay: 50,
+    deleteSpeed: 20
+});
